612df3ca404c513a275a95670a7c52cd98aeadca
Axelor <162461637+mah-axelor@users.noreply.github.com>
2024-04-05 16:01:41 +0200

    wip-77137: typo in translation and add a line break

--- axelor-sale/src/main/java/com/axelor/apps/sale/db/repo/SaleOrderLineSaleRepository.java
+++ axelor-sale/src/main/java/com/axelor/apps/sale/db/repo/SaleOrderLineSaleRepository.java
@@ -33,6 +33,7 @@ public class SaleOrderLineSaleRepository extends SaleOrderLineRepository {
         "$nbDecimalDigitForUnitPrice",
         Beans.get(AppBaseService.class).getNbDecimalDigitForUnitPrice());
     json.put("$nbDecimalDigitForQty", Beans.get(AppBaseService.class).getNbDecimalDigitForQty());
+
     if (context.get("_model") != null
         && (context.get("_model").equals(SaleOrder.class.getName())
             || context.get("_model").equals(SaleOrderLine.class.getName()))
